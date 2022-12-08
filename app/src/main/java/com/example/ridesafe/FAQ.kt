a692b3ec4fb497bf33cfd7ff222db655686ecdd2
eriesegui <qemcsegui@tip.edu.ph>
2022-12-09 03:45:07 +0800

    add FAQ

---
+++ app/src/main/java/com/example/ridesafe/FAQ.kt
@@ -0,0 +1,31 @@
+package com.example.ridesafe
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FAQ : AppCompatActivity() {
+    val recyclerView = findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.recyclerView)
+    val faqList = ArrayList<FAQFragment>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_faq)
+
+        initData()
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView() {
+        val faqAdapter = FAQAdapter(faqList)
+        recyclerView.adapter = faqAdapter
+        recyclerView.setHasFixedSize(true)
+    }
+
+    private fun initData() {
+        faqList.add(FAQFragment(
+            "What is ridesafe?",
+            "Lorem ipsum blah blah blah"
+        ))
+    }
+
+}
---
+++ app/src/main/java/com/example/ridesafe/FAQFragment.kt
@@ -0,0 +1,4 @@
+package com.example.ridesafe
+
+class FAQFragment(val question: String, val content: String, var expandable: Boolean = false) {
+}
