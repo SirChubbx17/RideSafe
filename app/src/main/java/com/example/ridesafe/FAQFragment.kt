c6fd142d40bf760d3ce2c3dea71c84e9452c9edc
SirChubbx17 <qjccdelacruz@tip.edu.ph>
2022-12-09 10:45:50 +0800

    Login and faq fix

--- app/src/main/java/com/example/ridesafe/FAQFragment.kt
+++ app/src/main/java/com/example/ridesafe/FAQFragment.kt
@@ -1,13 +1,11 @@
 package com.example.ridesafe
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
 class FAQFragment(val question: String, val content: String, var expandable: Boolean = false) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_faq, container, false)
-    }
 
 }
