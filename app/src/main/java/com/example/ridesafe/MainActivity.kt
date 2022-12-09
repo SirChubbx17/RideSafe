cd7b0625752bb4fb9ecc82dbc4b84dd4eecbef5c
SirChubbx17 <qjccdelacruz@tip.edu.ph>
2022-12-09 15:23:57 +0800

    Nav Drawer fix

--- app/src/main/java/com/example/ridesafe/MainActivity.kt
+++ app/src/main/java/com/example/ridesafe/MainActivity.kt
@@ -14,58 +14,78 @@ import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var toggle: ActionBarDrawerToggle
-    lateinit var drawerLayout: DrawerLayout
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        drawerLayout = findViewById(R.id.drawerLayout)
-        val navView : NavigationView = findViewById(R.id.navView)
-
-        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
+        drawerLayout = binding.drawerLayout
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        navView.setNavigationItemSelectedListener {
-            it.isChecked = true
-            when (it.itemId) {
-                R.id.aboutFragment -> replaceFragment(AboutFragment(), it.title.toString())
-                R.id.aboutFragment -> replaceFragment(AboutFragment(), it.title.toString())
-
-//                    R.id.FAQFragment -> {
-//                        replaceFragment(FAQ(), it.title.toString())
-//                    }
-            }
-            true
-        }
-
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)){
-            true
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun replaceFragment(fragment: Fragment, title: String){
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.myNavHostFragment,fragment)
-        fragmentTransaction.commit()
-        drawerLayout.closeDrawers()
-        setTitle(title)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return NavigationUI.navigateUp(navController,drawerLayout)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
+
+//    lateinit var toggle: ActionBarDrawerToggle
+//    lateinit var drawerLayout: DrawerLayout
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        drawerLayout = findViewById(R.id.drawerLayout)
+//        val navView : NavigationView = findViewById(R.id.navView)
+//
+//        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+//        drawerLayout.addDrawerListener(toggle)
+//        toggle.syncState()
+//
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//
+//        navView.setNavigationItemSelectedListener {
+//            it.isChecked = true
+//            when (it.itemId) {
+//                R.id.homeFragment -> replaceFragment(HomeFragment(), it.title.toString())
+//                R.id.aboutFragment -> replaceFragment(AboutFragment(), it.title.toString())
+//
+////                    R.id.FAQFragment -> {
+////                        replaceFragment(FAQ(), it.title.toString())
+////                    }
+//            }
+//            true
+//        }
+//
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        if (toggle.onOptionsItemSelected(item)){
+//            true
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
+//
+//    private fun replaceFragment(fragment: Fragment, title: String){
+//        val fragmentManager = supportFragmentManager
+//        val fragmentTransaction = fragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.myNavHostFragment,fragment)
+//        fragmentTransaction.commit()
+//        drawerLayout.closeDrawers()
+//        setTitle(title)
+//    }
+//
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = this.findNavController(R.id.myNavHostFragment)
+//        return NavigationUI.navigateUp(navController,drawerLayout)
+//    }
+//}
 
 //    override fun onSupportNavigateUp(): Boolean {
 //        val navController = this.findNavController(R.id.myNavHostFragment)
