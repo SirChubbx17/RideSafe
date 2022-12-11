0dac51f96bd1938b0bb2f2ab42fc3a81c66e6006
SirChubbx17 <qjccdelacruz@tip.edu.ph>
2022-12-12 02:24:08 +0800

    medyo gumagana bot nav

--- app/src/main/java/com/example/ridesafe/Home.kt
+++ app/src/main/java/com/example/ridesafe/Home.kt
@@ -22,6 +22,16 @@ class Home : Fragment() {
             false
         )
 
+        binding.bottomNav.setOnItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.homeId -> {replaceFragment(Home(), it.title.toString())}
+                R.id.historyId -> {replaceFragment(History(), it.title.toString())}
+                //R.id.help -> { replaceFragment(FAQFragment(), it.title.toString()) }
+            }
+            true
+        }
+
         setHasOptionsMenu(true)
         return binding.root
 
@@ -35,6 +45,13 @@ class Home : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return NavigationUI.onNavDestinationSelected(item,requireView().findNavController())
                 || super.onOptionsItemSelected(item)
+    }
+
+    private fun replaceFragment(fragment: Fragment, title: String){
+        val fragmentManager = childFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
     }
 
 }
