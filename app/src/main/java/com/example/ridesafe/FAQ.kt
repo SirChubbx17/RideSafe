561c0a36403483baf21c399c5281603b50f4e791
eriesegui <qemcsegui@tip.edu.ph>
2022-12-13 00:41:06 +0800

    updated faq

--- app/src/main/java/com/example/ridesafe/FAQ.kt
+++ app/src/main/java/com/example/ridesafe/FAQ.kt
@@ -29,13 +29,19 @@ class FAQ : AppCompatActivity() {
         faqList.add(
             FAQData(
             "What is ridesafe?",
-            "Lorem ipsum blah blah blah"
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est placerat in egestas erat. Semper feugiat nibh sed pulvinar proin gravida hendrerit. Malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit. Enim nulla aliquet porttitor lacus luctus accumsan."
             )
         )
         faqList.add(
             FAQData(
-                "What is ridesafe?",
-                "Lorem ipsum blah blah blah"
+                "How to user ridesafe?",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            )
+        )
+        faqList.add(
+            FAQData(
+                "Fix a map issue",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
             )
         )
     }
